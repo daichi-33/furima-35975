ab7a6f6cf595b87320e587c3274448b6c5d919c7
daichi-33 <knz.bgnl@gmail.com>
2021-07-02 15:22:40 +0900

    ユーザー管理機能の編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :buyer_records
+
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :birthday
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
 end
