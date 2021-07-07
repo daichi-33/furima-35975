da7cc958aad4dc901b7ce264dd16adfdb1a3ab94
daichi-33 <knz.bgnl@gmail.com>
2021-07-07 15:12:29 +0900

    パスコードの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,7 +30,7 @@ class ItemsController < ApplicationController
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
-      render root_path
+      render :edit
     end
   end
 
