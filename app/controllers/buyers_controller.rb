a7c89118dabc8b78e64f34c90ce283f577d83d47
daichi-33 <knz.bgnl@gmail.com>
2021-07-11 18:45:02 +0900

    クレジットカード決済機能の導入

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -11,9 +11,10 @@ class BuyersController < ApplicationController
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @buyer_delivery_info = BuyerDeliveryInfo.new(buyer_params)
+    @item = Item.find(params[:item_id])
     if @buyer_delivery_info.valid?
+      pay_item
       @buyer_delivery_info.save
       redirect_to root_path
     else
@@ -24,6 +25,16 @@ class BuyersController < ApplicationController
   private
 
   def buyer_params
-    params.require(:buyer_delivery_info).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:buyer_delivery_info).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, 
+                                                :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_dec990f80fa0028197f50198"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: buyer_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -2,4 +2,6 @@ class Buyer < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :delivery_info
+
+  attr_accessor :token
 end
